bc97b2d6bf66956b0c39aca7be6ba8d8bbf57374
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-04-07 20:10:19 +0200

    second commit

--- Index.js
+++ Index.js
@@ -1,15 +1,15 @@
 
-  let icons = ["🍎", "🍑", "🍓", "🍇", "🍌", "🍍"];
+  let icons = ["🍎", "🍑", "🍓", "🍇", "🍌", "7️⃣" ];
  
 class App extends React.Component {
 
     render() {
-      let random1 = Math.floor(Math.random() * 5); 
-      let random2 = Math.floor(Math.random() * 5);
-      let random3 = Math.floor(Math.random() * 5);
+      let random1 = Math.floor(Math.random() * 6); 
+      let random2 = Math.floor(Math.random() * 6);
+      let random3 = Math.floor(Math.random() * 6);
       return (
         <div>
-          <h1>Slot Machines!</h1>
+          <h1 style={{textAlign: "center", backgroundColor: "yellow"}}>Slot Machines!</h1>
           <Machine
             s1={icons[random1]} 
             s2={icons[random2]}
--- Machine.js
+++ Machine.js
@@ -5,14 +5,25 @@ class Machine extends React.Component {
       const div = {
         display: "flex",
         justifyContent: "center",
-        border: "3px solid red"
+        height: '300px',
+        border: "10px solid red"
       }
   
       return (
         <div>
-          <div Style={div}><p style={{fontSize: '80px'}}>{s1} {s2} {s3}</p></div>
+          <div style={div}><p style={{fontSize: '120px', alignItems: "space-between"}}>{s1} {s2} {s3}</p></div>
           
-          <p>{winner ? 'Winner!' : 'Loser!'}</p>
+          <p>{winner 
+           ?
+           <div>
+           <img style={{display:'block', margin:'auto'}} src='https://image.shutterstock.com/image-photo/nice-little-cat-cup-260nw-171497567.jpg'/>
+            <h1 style={{textAlign:'center'}}>WINNER!!!</h1>
+           </div>
+           :
+           <div>
+            <img style={{display:'block', margin:'auto'}} src='https://res.cloudinary.com/teepublic/image/private/s--w4sLxT6n--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:38/co_ffffff,e_outline:inner_fill:38/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1528373025/production/designs/2764794_0.jpg'/>
+           </div> 
+           }</p>
         </div>
       )
     }
